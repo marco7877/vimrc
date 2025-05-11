d6d5c9112b97eb7d349448ddece087ef5c777ba0
marco7877 <marcoantonio7877@gmail.com>
2025-05-11 20:18:09 +0200

    working init bash script

--- init.sh
+++ init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-cp -s ./.vimrc ~/.vimrc
+repo_dir=$(pwd)
+cd ~
+home=$(pwd)
+vim_file=/.vimrc
+if test -f "${home}${vim_file}";then
+	read -p "${home}${vim_file} exist! Overwrite it? yes/no" ans
+	if [[ ${ans} == "yes" ]];then
+	rm ${home}${vim_file}
+	cp -s ${repo_dir}${vim_file} ${home}${vim_file}
+	fi
+else
+	cp -s ${repo_dir}${vim_file} ${home}${vim_file}
+fi
